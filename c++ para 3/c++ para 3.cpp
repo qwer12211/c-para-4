bc8ba642bcd718c04d55985188552b65b119978d
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2024-02-18 16:21:28 +0300

    Update c++ para 3.cpp

--- c++ para 3/c++ para 3.cpp
+++ c++ para 3/c++ para 3.cpp
@@ -1,28 +1,28 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    const int ROWS = 3;
-    const int COLS = 5;
-    double arr[ROWS][COLS];
+    const int dorosgka = 3;
+    const int stolbech = 5;
+    double arr[dorosgka][stolbech];
 
     // Ввод элементов массива с клавиатуры
-    for (int i = 0; i < ROWS; i++) {
+    for (int i = 0; i < dorosgka; i++) {
         cout << "Введите элементы строки " << i + 1 << " через пробел ";
-        for (int j = 0; j < COLS; j++) {
+        for (int j = 0; j < stolbech; j++) {
             cin >> arr[i][j];
         }
     }
 
     // Вычисление среднего арифметического для каждой строки
-    for (int i = 0; i < ROWS; i++) {
+    for (int i = 0; i < dorosgka; i++) {
         double sum = 0;
-        for (int j = 0; j < COLS; j++) {
+        for (int j = 0; j < stolbech; j++) {
             sum += arr[i][j];
         }
-        double average = sum / COLS;
+        double average = sum / stolbech;
         cout << "Среднее арифметическое равно " << i + 1 << ": " << average << endl;
     }
 
