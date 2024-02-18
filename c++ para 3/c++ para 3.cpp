1ca554546bd8462e39de347826bd8d2a90771608
Misha <Misha@DESKTOP-PJPTSCI>
2024-02-18 16:16:56 +0300

    Добавьте файлы проекта.

---
+++ c++ para 3/c++ para 3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    const int ROWS = 3;
+    const int COLS = 5;
+    double arr[ROWS][COLS];
+
+    // Ввод элементов массива с клавиатуры
+    for (int i = 0; i < ROWS; i++) {
+        cout << "Введите элементы строки " << i + 1 << " через пробел ";
+        for (int j = 0; j < COLS; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    // Вычисление среднего арифметического для каждой строки
+    for (int i = 0; i < ROWS; i++) {
+        double sum = 0;
+        for (int j = 0; j < COLS; j++) {
+            sum += arr[i][j];
+        }
+        double average = sum / COLS;
+        cout << "Среднее арифметическое равно " << i + 1 << ": " << average << endl;
+    }
+
+    return 0;
+}
